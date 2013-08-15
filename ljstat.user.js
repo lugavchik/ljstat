1d4bc959f0b6f26673d036c787e1989aea42ad40
Alexander <git@lugovov.ru>
2013-08-15 19:26:08 +0400

    Реализована функция скрытия ользователей из статистики.

--- ljstat.user.js
+++ ljstat.user.js
@@ -11,7 +11,8 @@
 (function(){
 var run=function(){jQuery(function($){
     var users={0:{id:0,name:'-',comments:0,cbd:{}}}
-	var options={limit:10};
+	var current_user=$('#user').val();
+	var options={limit:10,ignore:current_user};
     var CheckUser=function(){
         if (!users[$(this).attr('id')]){
             users[$(this).attr('id')]={id:$(this).attr('id'),name:$(this).attr('user'),comments:0,cbd:{}}
@@ -43,10 +44,17 @@ var run=function(){jQuery(function($){
     var SortUsers=function(a,b){
              return b.comments-a.comments;   
     }
-    var GetULink=function(user){
-        if (user=='-')
-            return '<b>Аноним</b>';
-        return '<span class="ljuser i-ljuser" lj:user="'+user+'"><a>'+user+'</a></span>';    
+    var GetULink=function(user,lite){
+		if (lite){
+	        if (user=='-')
+	            return '<b>Аноним</b>';
+	        return '<lj user="'+user+'"/>';    
+		}else{
+			var close='<span class="addremove" data-u="'+user+'">[x]</span>';
+	        if (user=='-')
+	            return '<b>Аноним</b>'+close;
+	        return '<span class="ljuser i-ljuser" lj:user="'+user+'"><a>'+user+'</a></span>'+close;    
+		}
     }
     var GetProgress=function(c,m,s){
         return '<img src="http://l-stat.livejournal.com/img/poll/leftbar.gif" height="14"/>'+
@@ -64,37 +72,93 @@ var run=function(){jQuery(function($){
         text='';
         max=p[0].comments;
 		summ=0;
+		hide=0;
         $(p).each(function(){
-            summ+=this.comments;
-               });
+			if (options.ignore[this.name]){
+				hide+=this.comments;
+			}else{
+	            summ+=this.comments;
+			}
+        });
 		var limit=0;
         $(p).each(function(){
-            if (this.comments>0&&limit++<options.limit){
+			if (!options.ignore[this.name])
+	            if (this.comments>0&&limit++<options.limit){
                     text+=GetLine(this,limit,max,summ);
 				}
-               });
-        $('#lj-comm-table').html('<table>'+text+'</table>');
+        });
+        $('#lj-comm-table').html('<table>'+text+'</table><div>Всего комментариев: <b id="lj-comm-allc"></b><br/>'+(hide?'Из них скрыто: <b id="lj-comm-hidec"></b>':'')+'</div>')
+			.find('#lj-comm-allc').html(summ+hide).end()
+			.find('#lj-comm-hidec').html(hide).end()
+			;
 
     }
-	var GetOption=function(name){
-		return options[name]=localStorage['lj-comm-'+name]|options[name];
+	var LoadIgnoreList=function(){
+		var ign=GetOption('ignore');
+		options.ignore={};
+		try{
+			if (ign>""){
+				var ign=ign.split('|');
+				for(var i in ign)
+					if (ign.hasOwnProperty(i))
+						options.ignore[ign[i]]=true;
+			}
+		}catch(e){
+			console.log('Error Loading Ignore List');
+		}
 	}
-	var SetOption=function(name,val){
-		options[name]=val;
-		localStorage.setItem('lj-comm-'+name,val);
+
+	var SaveIgnoreList=function(){
+		var ign=[];
+		for(var i in options.ignore)
+			if (options.ignore.hasOwnProperty(i))
+				ign.push(i);
+		SetOption('ignore',ign.join('|'),true);
+	}
+	var GetOption=function(name){
+		return options[name]=localStorage['lj-comm-'+current_user+'-'+name]||options[name];
+	}
+	var SetOption=function(name,val,noreplace){
+		if(!noreplace)
+			options[name]=val;
+		localStorage.setItem('lj-comm-'+current_user+'-'+name,val);
+	}
+	var AddRemoveUser=function(){
+		var u=$(this).data('u');
+		if (options.ignore.hasOwnProperty(u))
+			delete options.ignore[u];
+		else
+			options.ignore[u]=true;
+		PrintIgnoreList();
+		SaveIgnoreList();
+		Print();
+	}
+	var PrintIgnoreList=function(){
+		var a=[];
+		for(var i in options.ignore)
+			if (options.ignore.hasOwnProperty(i))
+				a.push(GetULink(i));
+		$('#lj-comm-hideusers').html(a.join(', ')+'.');
 	}
     var ShowForm=function(){
-        var div=$('<div><div id="lj-comm-form"></div><div id="lj-comm-table"></div>Загружаем</div>').prependTo('#Content').css({position:'relative','background-color':'#fff','border':'1px solid silver','z-index':19});
+        var div=$('<div><div id="lj-comm-form"></div><div id="lj-comm-table">Загружаем данные</div></div>').prependTo('#Content')
+			.css({position:'relative','background-color':'#fff','border':'1px solid silver','z-index':19})
+			.delegate('.addremove','click',AddRemoveUser);
 		CreateForm();
         LoadComments(0);
 		button.fadeOut(function(){$(this).remove()})
     }
 	var CreateForm=function(){
-		$('<table><tr><td>Показать не более:</td><td><div id="lj-comm-limitdiv"><input type="number" id="lj-comm-limit" min="5" step="5" max="500"/><br/><span>10</span><span>15</span><span>25</span><span>50</span><span>100</span></div></td></tr><tr><td>Скрытые пользователи:</td><td><div id="lj-comm-hideusers"></div></td></tr></table>').appendTo('#lj-comm-form')
+		$('head').append('<style>.addremove{cursor:pointer;top:-5px;position:relative;font-size:8pt;transition:1s;} .addremove:hover{color:red;background-color:#fcc;}</style>')
+		$('<table><tr><td>Показать не более:</td><td><div id="lj-comm-limitdiv"><input type="number" id="lj-comm-limit" min="5" step="5" max="500"/><span>10</span><span>15</span><span>25</span><span>50</span><span>100</span></div></td></tr><tr><td>Скрытые пользователи:</td><td><div id="lj-comm-hideusers"></div></td></tr></table>').appendTo('#lj-comm-form')
 		.find('#lj-comm-limitdiv span').css({padding:'5px',color:'blue',cursor:'pointer'}).click(function(){$('#lj-comm-limit').val($(this).text()).change()}).end()
 		.find('#lj-comm-limit').val(GetOption('limit')).change(function(){SetOption('limit',$(this).val());Print()}).end();
+		PrintIgnoreList();
+		
 	}
     var button=$('<button/>').css({position:'fixed',top:'10px',right:'10px'}).text('Загрузить статистику').click(ShowForm).prependTo('#Content');
+	LoadIgnoreList();
+
 });
 }
 if(typeof(jQuery)=='undefined'){
